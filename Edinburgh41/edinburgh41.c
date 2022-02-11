2c510d70c72d9977a6f1703adddec95274d8862e
schwarzer-geiger <lalitmistry1407@gmail.com>
2022-02-11 10:44:11 +0000

    Update and rename reviung41.c to edinburgh41.c

--- Edinburgh41/edinburgh41.c
+++ Edinburgh41/edinburgh41.c
@@ -13,4 +13,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "reviung41.h"
+#include "edinburgh41.h"
